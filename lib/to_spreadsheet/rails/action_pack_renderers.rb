20078712051d3e0fc8462002789ed4834cfb417d
quasi-presence <quasi_presence@yahoo.com>
2013-08-22 13:44:02 -0700

    Attempt to render template file and then partial file

--- lib/to_spreadsheet/rails/action_pack_renderers.rb
+++ lib/to_spreadsheet/rails/action_pack_renderers.rb
@@ -6,8 +6,13 @@ require 'action_controller/metal/responder'
 # This is similar to how Rails internally implements its :json and :xml renderers
 ActionController::Renderers.add :xlsx do |template, options|
   filename = options[:filename] || options[:template] || 'data'
+  xlsx_options = options.merge(layout: false, formats: [:xlsx, :html])
   data = ToSpreadsheet::Context.with_context ToSpreadsheet::Context.global.merge(ToSpreadsheet::Context.new) do |context|
-    html = render_to_string(template, options.merge(template: template.to_s, formats: ['xlsx', 'html']))
+    begin
+      html = render_to_string(xlsx_options.merge(template: template.to_s))
+    rescue ActionView::MissingTemplate => ex
+      html = render_to_string(xlsx_options.merge(partial: template.to_s))
+    end
     ToSpreadsheet::Renderer.to_data(html, context)
   end
   send_data data, type: :xlsx, disposition: %(attachment; filename="#{filename}.xlsx")
